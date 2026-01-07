5f85c10d02438dba41a5f7df27870f45cb1d8e93
rexwithluv <aaronaax@gmail.com>
2026-01-07 22:34:58 +0100

    feat: send message only when data change

--- src/jobs.py
+++ src/jobs.py
@@ -5,12 +5,18 @@ from .scrapers.sergas import check_cupo_medico_sergas
 
 
 async def medico_job(context: ContextTypes.DEFAULT_TYPE):
-    chat_id = context.job.chat_id
+    chat_id = context.bot_data.get("config").chat_id
     resultado = await check_cupo_medico_sergas()
-    await context.bot.send_message(chat_id=chat_id, text=resultado)
+
+    if resultado != context.bot_data.get("cupo_medico"):
+        await context.bot.send_message(chat_id=chat_id, text=resultado)
+        context.bot_data["cupo_medico"] = resultado
 
 
 async def iphone_job(context: ContextTypes.DEFAULT_TYPE):
-    chat_id = context.job.chat_id
+    chat_id = context.bot_data.get("config").chat_id
     resultado = await get_iphone_stock()
-    await context.bot.send_message(chat_id=chat_id, text=resultado)
+
+    if resultado != context.bot_data.get("iphone_stock"):
+        await context.bot.send_message(chat_id=chat_id, text=resultado)
+        context.bot_data["iphone_stock"] = resultado
